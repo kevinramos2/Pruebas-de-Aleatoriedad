403fdc849594dd7fe5db9fa8c977cd4f52ed2e70
kevinramos2 <keramosl@unal.edu.co>
2024-12-01 13:51:38 -0500

    Update loteria.py

--- loteria.py
+++ loteria.py
@@ -12,11 +12,6 @@ with open(ruta_archivo, 'r') as read_obj:
         v = int(row[0])  # Convertir a entero
         resultadosLoteria.append(v)
 
-#Prueba 1 - Frecuencia de Números Pares/Impares usando Chi-cuadrado
-
-# Ho: La cantidad de números pares e impares es igual
-# Ha: La cantidad de números pares e impares es diferente
-
 def paresImpares(resultadosLoteria):
   pares = 0
   impares = 0
@@ -28,24 +23,33 @@ def paresImpares(resultadosLoteria):
   return pares, impares
 
 def pruebaFinal(estadístico, valorC):
+  print(f'El estadístico es {estadístico}')
+  print(f'El valor crítico es {valorC}\n')
   if estadístico <= valorC:
-    print("No hay suficiente evidencia para rechazar Ho, por lo tanto la cantidad de pares e impares en esta secuencia se puede considerar igual.")
+    print(f'Como {estadístico:.2f} <= {valorC:.2f}:')
+    print("No hay suficiente evidencia para rechazar Ho, por lo tanto la cantidad de pares e impares en esta secuencia se puede considerar igual.\n")
   else:
-    print("Hay suficiente evidencia para rechazar Ho, por lo tanto, aceptamos Ha y sabemos que la cantidad de pares e impares en esta secuencia es diferente.")
+    print(f'Como {estadístico:.2f} > {valorC:.2f}:')
+    print("Hay suficiente evidencia para rechazar Ho, por lo tanto, aceptamos Ha y sabemos que la cantidad de pares e impares en esta secuencia es diferente.\n")
 
-
+#Prueba 1 - Frecuencia de Números Pares/Impares usando Chi-cuadrado
 p,i = paresImpares(resultadosLoteria)
-print(f'En {len(resultadosLoteria)} datos hay {p} pares y {i} impares')
+print()
+print("Se trabajará con un CSV que contiene cierta cantidad de resultados de la lotería de Bogotá.")
+print("Las hipótesis planteadas son:")
+print("Ho: La cantidad de números pares e impares es igual")
+print("Ha: La cantidad de números pares e impares es diferente\n")
+print(f'En los {len(resultadosLoteria)} resultados de la lotería, hay {p} resultados pares y {i} impares\n')
 
 Ei = len(resultadosLoteria)//2 #Frecuencia esperada
 pruebaChi2 = ((p-Ei)**2/Ei) + ((i-Ei)**2/Ei) #Prueba chi-cuadrado para obtener el estadístico
 alpha = 0.05 #Significancia
-gl = 1 #Grados de libertad
+gl = 1 #Grados de libertad = categorías(par/impar) - 1 = 2 - 1 = 1
 valorCritico = chi2.ppf(1 - alpha, gl)
 
-print(f'El estadístico es {pruebaChi2}')
-print(f'El valor crítico es {valorCritico}')
 pruebaFinal(pruebaChi2,valorCritico)
+
+#Prueba 2 - Prueba de frecuencia para uniformidad usando Chi-cuadrado
 
 
  
