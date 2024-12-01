17c3b4a55597c4718b6692cad2f8ebd93fdbbe4b
kevinramos2 <keramosl@unal.edu.co>
2024-12-01 18:45:42 -0500

    Update RachasAscendentesDescendentes.py

--- loteria/RachasAscendentesDescendentes.py
+++ loteria/RachasAscendentesDescendentes.py
@@ -13,28 +13,29 @@ with open(rutaArchivo, 'r') as read_obj:
         resultadosLoteria.append(v)
 
 def rachas(resultados):
-  ascendente = 0
-  descendente = 0
-  for i in range(len(resultados)):
-      anterior = resultados[i-1]
-      siguiente = resultados[i]
-      if ascendente == 0:
-        ascendente += 1
-      else:
-        if siguiente > anterior:
-          ascendente += 1
-        else:
-          descendente += 1
-  return ascendente, descendente
+  corridasAscendentes = 0
+  corridasDescendentes = 0
+  tendenciaActual = None  # Puede ser 'ascendente' descendente', o None
+  for i in range(1, len(resultados)):
+      if resultados[i] > resultados[i - 1]:  # Ascendente
+          if tendenciaActual != "ascendente":
+              corridasAscendentes += 1
+              tendenciaActual = "ascendente"
+      elif resultados[i] < resultados[i - 1]: # Descendente
+          if tendenciaActual != "descendente":
+              corridasDescendentes += 1    
+              tendenciaActual = "descendente"
+  return corridasAscendentes, corridasDescendentes
 
+def prueba(asc,des,tamaño):
+   Ei = tamaño/2 #Frecuencia esperada
+   return
 #Prueba 3 - Pruebas de rachas ascendentes/descendentes para independencia
 print()
 print("Se trabajará con un CSV que contiene cierta cantidad de resultados de la lotería de Bogotá.")
 print("Las hipótesis planteadas son:")
 print("Ho: Los números están en una secuencia independiente.")
 print("Ha: Los números no están en una secuencia independiente.\n")
-print(rachas(resultadosLoteria))
-
-
-#ascendente, descendente = rachas(resultados)
-#print(f"Rachas ascendentes: {ascendente}, Rachas descendentes: {descendente}")
+asc,desc = rachas(resultadosLoteria)
+print(f"Rachas ascendentes: {asc}, Rachas descendentes: {desc}")
+#prueba(asc,desc,len(resultadosLoteria))
