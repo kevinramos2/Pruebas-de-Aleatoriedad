36c2911487d9caeaf6fdaec944a650c2f13eef80
kevinramos2 <keramosl@unal.edu.co>
2024-12-01 17:32:33 -0500

    Update RachasAscendentesDescendentes.py

--- loteria/RachasAscendentesDescendentes.py
+++ loteria/RachasAscendentesDescendentes.py
@@ -12,15 +12,29 @@ with open(rutaArchivo, 'r') as read_obj:
         v = int(row[0])  # Convertir a entero
         resultadosLoteria.append(v)
 
-def rachas(resutlados):
+def rachas(resultados):
   ascendente = 0
   descendente = 0
+  for i in range(len(resultados)):
+      anterior = resultados[i-1]
+      siguiente = resultados[i]
+      if ascendente == 0:
+        ascendente += 1
+      else:
+        if siguiente > anterior:
+          ascendente += 1
+        else:
+          descendente += 1
+  return ascendente, descendente
 
-  return
 #Prueba 3 - Pruebas de rachas ascendentes/descendentes para independencia
 print()
 print("Se trabajará con un CSV que contiene cierta cantidad de resultados de la lotería de Bogotá.")
 print("Las hipótesis planteadas son:")
 print("Ho: Los números están en una secuencia independiente.")
 print("Ha: Los números no están en una secuencia independiente.\n")
-rachas(resultadosLoteria)
+print(rachas(resultadosLoteria))
+
+
+#ascendente, descendente = rachas(resultados)
+#print(f"Rachas ascendentes: {ascendente}, Rachas descendentes: {descendente}")
