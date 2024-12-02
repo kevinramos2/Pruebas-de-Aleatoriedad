47195cc2ae0b67d7ad3d76560f7b9105b020f50f
kevinramos2 <keramosl@unal.edu.co>
2024-12-01 19:15:50 -0500

    Update RachasAscendentesDescendentes.py

--- loteria/RachasAscendentesDescendentes.py
+++ loteria/RachasAscendentesDescendentes.py
@@ -1,5 +1,6 @@
 import csv
 from scipy.stats import chi2
+import math as m
 
 # Lista para almacenar los resultados
 resultadosLoteria = []
@@ -16,7 +17,7 @@ def rachas(resultados):
   corridasAscendentes = 0
   corridasDescendentes = 0
   tendenciaActual = None  # Puede ser 'ascendente' descendente', o None
-  for i in range(1, len(resultados)):
+  for i in range(len(resultados)):
       if resultados[i] > resultados[i - 1]:  # Ascendente
           if tendenciaActual != "ascendente":
               corridasAscendentes += 1
@@ -28,8 +29,16 @@ def rachas(resultados):
   return corridasAscendentes, corridasDescendentes
 
 def prueba(asc,des,tamaño):
-   Ei = tamaño/2 #Frecuencia esperada
-   return
+  a = asc+des
+  miu = ((2*tamaño)-1)/3 #Frecuencia esperada
+  sigmaC = ((16*tamaño)-29)/90 #Desviación
+  estadistico = (a-miu)/m.sqrt(sigmaC) #Estadístico estandarizado
+
+  if abs(estadistico) <= 1.96:
+    print(f'Como el estadístico calculado ({abs(estadistico):.2f}) es <= al valor crítico (1.96), NO hay suficiente evidencia para descartar Ho, por lo tanto las observaciones son independientes.')
+  else:
+     print(f'Como el estadístico calculado ({abs(estadistico):.2f}) es > al valor crítico (1.96), hay suficiente evidencia para descartar Ho, por lo tanto las observaciones NO son independientes.')
+
 #Prueba 3 - Pruebas de rachas ascendentes/descendentes para independencia
 print()
 print("Se trabajará con un CSV que contiene cierta cantidad de resultados de la lotería de Bogotá.")
@@ -38,4 +47,4 @@ print("Ho: Los números están en una secuencia independiente.")
 print("Ha: Los números no están en una secuencia independiente.\n")
 asc,desc = rachas(resultadosLoteria)
 print(f"Rachas ascendentes: {asc}, Rachas descendentes: {desc}")
-#prueba(asc,desc,len(resultadosLoteria))
+prueba(asc,desc,len(resultadosLoteria))
