fe217da01cf38a6c9541cd9d1479d63cd6b6affc
kevinramos2 <keramosl@unal.edu.co>
2024-12-03 14:46:42 -0500

    Update paresImpares.py

--- loteria/paresImpares.py
+++ loteria/paresImpares.py
@@ -1,4 +1,5 @@
 import csv
+from matplotlib import pyplot as plt
 from scipy.stats import chi2
 
 # Lista para almacenar los resultados
@@ -55,3 +56,20 @@ valorCritico = chi2.ppf(1 - alpha, gl)
 
 # Presentación de resultados
 pruebaFinal(pruebaChi2, valorCritico)
+
+# Prueba espectral
+# Crear pares adyacentes
+pares = [(resultadosLoteria[i], resultadosLoteria[i+1]) for i in range(len(resultadosLoteria)-1)]
+
+# Separar en dos listas para graficar
+x_vals = [par[0] for par in pares]
+y_vals = [par[1] for par in pares]
+
+# Graficar los pares en un plano cartesiano
+plt.figure(figsize=(10, 6))
+plt.scatter(x_vals, y_vals, alpha=0.6, edgecolor='k', color='blue')
+plt.title("Prueba espectral: Pares adyacentes de la secuencia")
+plt.xlabel("x_j")
+plt.ylabel("x_{j+1}")
+plt.grid(True)
+plt.show()
